6f7e9c22112c34ca1e9b3c825e90474586e8a159
Muhammad El Zedy <mohamedelzedyfcih@gmail.com>
2019-08-21 15:15:13 +0200

    Initial Commit

---
+++ GoodEatins-V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins-V2
+//
+//  Created by Muhammad El Zedy on 8/21/19.
+//  Copyright © 2019 ZozShark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
